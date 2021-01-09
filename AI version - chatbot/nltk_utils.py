737c91b385a806b8ff9d529095b481f62793313b
Ankith Abhayan <ankithabhayan@gmail.com>
2021-01-09 15:31:46 +0530

    ai added

---
+++ AI version - chatbot/nltk_utils.py
@@ -0,0 +1,27 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+stemmer = PorterStemmer()
+
+def tokanize(sentence): #break sentence into words 
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word,lower())
+
+def bag(tokanized_sentence,allwords):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
